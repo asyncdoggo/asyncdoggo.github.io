9cb66f7dc947f7bd8b2768cfc0caa97cd61e406d
Ayush Deshpande <ayushdeshpande81@gmail.com>
2024-12-04 06:08:42 +0000

    Allow pyodide update

--- public/webworker.ts
+++ public/webworker.ts
@@ -1,9 +1,9 @@
 import {pyodideExpose, PyodideExtras} from "pyodide-worker-runner";
 import * as Comlink from "comlink";
-import {loadPyodide} from "pyodide";
+import { initPyodide, defaultPyodideLoader } from "pyodide-worker-runner";
 
-let pyodideReadyPromise = loadPyodide();
-
+let pyodideReadyPromise = defaultPyodideLoader();
+let initialized = false;
 
 Comlink.expose({
   runPython: pyodideExpose((extras, code) => {
@@ -20,10 +20,14 @@ const execute = async (extras: PyodideExtras, code: any) => {
   let result = ''
 
   const pyodide = await pyodideReadyPromise;
+  if (!initialized) {
+    initPyodide(pyodide);
+    initialized = true;
+  }
     if (extras.interruptBuffer) {  // i.e. if SharedArrayBuffer is available so this could be sent by the client
       pyodide.setInterruptBuffer(extras.interruptBuffer);
     }
-
+    await pyodide.loadPackagesFromImports(code);
     pyodide.setStdout({ batched: (msg: any) => { result += `${msg}\n` } });
     pyodide.setStdin({
       stdin: () => {
@@ -31,24 +35,34 @@ const execute = async (extras: PyodideExtras, code: any) => {
         return read;
       },
     })
-    const output = pyodide.runPython(code);    
-    if ((extras as any).inline && output){
-      result += output.toString();
-    }
 
-    let x: any = {};
-    const pyVariables = pyodide.globals.toJs();
-    for (let key in pyVariables) {
-      if(pyVariables[key] !== undefined) {
-        if (typeof pyVariables[key] === "number" || typeof pyVariables[key] === "string" || typeof pyVariables[key] === "boolean") {
-          x[key] = pyVariables[key];
-        }
-        else{
-          x[key] = pyVariables[key].toString();
-        }        
+
+    try{
+      const output = pyodide.runPython(code);   
+      if ((extras as any).inline && output){
+        result += output.toString();
       }
+  
+      let x: any = {};
+      const pyVariables = pyodide.globals.toJs();
+      for (let key in pyVariables) {
+        if(pyVariables[key] !== undefined) {
+          if (typeof pyVariables[key] === "number" || typeof pyVariables[key] === "string" || typeof pyVariables[key] === "boolean") {
+            x[key] = pyVariables[key];
+          }
+          else{
+            x[key] = pyVariables[key].toString();
+          }        
+        }
+      }
+  
+  
+      return {result, variables: x}; 
     }
-
-
-    return {result, variables: x};
+    catch(e){
+      result += e.toString();
+      return {result, variables: {}};
+    }
+        
+    
   }
